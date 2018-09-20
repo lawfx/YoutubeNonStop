b82efd3721213ea74bb3d60e67f2dae2371d9710
Nikolaos Ioannou <ioannounikos93@gmail.com>
2018-09-20 23:25:12 +0200

    chrome's starting example slightly modified

---
+++ autoconfirm.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({autoconfirm: 'true'}, function() {
+      console.log('Autoconfirm is enabled.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'youtube.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
---
+++ popup.js
@@ -0,0 +1,15 @@
+let changeColor = document.getElementById('changeColor');
+
+  chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+  });
+  
+  changeColor.onclick = function(element) {
+     let color = element.target.value;
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+       chrome.tabs.executeScript(
+           tabs[0].id,
+           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+     });
+   };
