1ede64b2045353a5848e5e38622a5d078ada0c62
Nikolaos Ioannou <ioannounikos93@gmail.com>
2018-09-21 00:34:46 +0200

    fixed youtube link, now it is recognized correctly - updated icons, name and description

--- autoconfirm.js
+++ autoconfirm.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'youtube.com'},
+          pageUrl: {hostEquals: 'www.youtube.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
