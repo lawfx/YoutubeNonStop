e3c598d8e056b53e2c6979580e3d69f0c44b6349
Nikolaos Ioannou <ioannounikos93@gmail.com>
2018-09-24 23:32:37 +0200

    fixed timer bug

--- autoconfirm.js
+++ autoconfirm.js
@@ -5,6 +5,7 @@ var ynsTag = "[YouTube NonStop] ";
 var lastClickTime = null;
 
 window.onload=function(){
+  lastClickTime = new Date().getTime();
   ynsInterval = setInterval(autoClicker, ynsIntervalTimer);
   console.info(ynsTag + "Monitoring YouTube for confirmation popup...");
 }
@@ -13,33 +14,44 @@ $(document).click(function() {
   lastClickTime = new Date().getTime();
 });
 
-function tryClickPaperToast(){
-  if($('paper-toast.paper-toast-open').find('#action-button').length){
-    $('paper-toast.paper-toast-open').find('#action-button').click();
-    console.debug(ynsTag+"Confirmed watching in toast!");
+function hasPoppedAfterTimeThreshold(){
+  var currTime = new Date().getTime();
+  if(currTime - lastClickTime <= clickTimeThreshold){
+    lastClickTime = new Date().getTime();
+    return false;
+  }
+  return true;
+}
+
+function tryClickPaperToast(parent){
+  if(parent.find('paper-toast.paper-toast-open').find('#action-button').length){
+    if(!hasPoppedAfterTimeThreshold()){
+      return;
+    }
+    parent.find('paper-toast.paper-toast-open').find('#action-button').click();
+    console.debug(ynsTag + "Confirmed watching in toast!");
   }
 }
 
-function tryClickPaperDialog(){
-  var paperDialogs = $('paper-dialog').filter(function(){
-    return $(this).css('display') != 'none';
-  });
-  paperDialogs.each(function() {
-    if($(this).find('#confirm-button').length){
-      $(this).find('#confirm-button').click();
-      console.debug(ynsTag + "Confirmed watching in dialog!");
+function tryClickPaperDialog(parent){
+  var paperDialog = parent.find('paper-dialog');
+  if(paperDialog.length){
+    if(paperDialog.css('display') != 'none'){
+      if(!hasPoppedAfterTimeThreshold()){
+        return;
+      }
+      if(paperDialog.find('#confirm-button').length){
+        paperDialog.find('#confirm-button').click();
+        console.debug(ynsTag + "Confirmed watching in dialog!");
+      }
     }
-  });
+  }
 }
 
 function autoClicker() {
   if(window.location.pathname === '/watch'){
-    var currTime = new Date().getTime();
-    if(currTime - lastClickTime <= clickTimeThreshold){
-      lastClickTime = new Date().getTime();
-      return;
-    }
-    tryClickPaperToast();
-    tryClickPaperDialog();
+    var parent = $('ytd-popup-container');
+    tryClickPaperToast(parent);
+    tryClickPaperDialog(parent);
   }
 }
