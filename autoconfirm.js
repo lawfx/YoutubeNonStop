d25a72bf4256c712add91f54ab35f155ebb5750a
Nikolaos Ioannou <ioannounikos93@gmail.com>
2018-09-25 19:40:47 +0200

    moved interval part to web worker

--- autoconfirm.js
+++ autoconfirm.js
@@ -1,11 +1,10 @@
-var ynsInterval = null;
-var ynsIntervalTimer = 1000;
+var ynsTag = "[Youtube NonStop] ";
 var clickTimeThreshold = 3000;
-var lastClickTime = null;
+var lastClickTime = new Date().getTime();
 
-lastClickTime = new Date().getTime();
-ynsInterval = setInterval(autoClicker, ynsIntervalTimer);
-postMessage("Monitoring YouTube for confirmation popup...");
+$(document).click(function() {
+  lastClickTime = new Date().getTime();
+});
 
 function hasPoppedAfterTimeThreshold(){
   var currTime = new Date().getTime();
@@ -16,39 +15,55 @@ function hasPoppedAfterTimeThreshold(){
   return true;
 }
 
-function tryClickPaperToast(parent){
-  if(parent.find("paper-toast.paper-toast-open").find("#action-button").length){
-    if(!hasPoppedAfterTimeThreshold()){
-      return;
-    }
-    parent.find("paper-toast.paper-toast-open").find("#action-button").click();
-    postMessage("Confirmed watching in toast!");
-  }
-}
-
-function tryClickPaperDialog(parent){
-  var paperDialog = parent.find("paper-dialog");
+function tryClickPaperDialog(){
+  var paperDialog = $('ytd-popup-container').find('paper-dialog');
   if(paperDialog.length){
-    if(paperDialog.css("display") != "none"){
+    if(paperDialog.css('display') != 'none'){
       if(!hasPoppedAfterTimeThreshold()){
         return;
       }
-      if(paperDialog.find("#confirm-button").length){
-        paperDialog.find("#confirm-button").click();
-        postMessage("Confirmed watching in dialog!");
+      if(paperDialog.find('#confirm-button').length){
+        paperDialog.find('#confirm-button').click();
+        console.debug(ynsTag + "Confirmed watching in dialog!");
       }
     }
   }
 }
 
-self.onmessage = function(e){
-  postMessage(e.data);
-};
+window.onload=function(){
+  if (typeof(Worker) !== "undefined") {
 
-function autoClicker() {
-  if(window.location.pathname === "/watch"){
-    var parent = $("ytd-popup-container");
-    //tryClickPaperToast(parent);
-    tryClickPaperDialog(parent);
+    var response = `var ynsInterval = null;
+    var ynsIntervalTimer = 1000;
+
+    ynsInterval = setInterval(whipWorker, ynsIntervalTimer);
+    postMessage("Monitoring YouTube for confirmation popup...");
+
+    function whipWorker(){
+      postMessage("whip");
+    }`;
+
+    var blob;
+    try {
+        blob = new Blob([response], {type: 'application/javascript'});
+    } catch (e) { // Backwards-compatibility
+        blob = new BlobBuilder();
+        blob.append(response);
+        blob = blob.getBlob();
+    }
+
+    var worker = new Worker(URL.createObjectURL(blob));
+
+      worker.onmessage = function(e){
+        if(e.data === "whip"){
+          tryClickPaperDialog();
+        }
+        else{
+          console.log(ynsTag + e.data);
+        }
+      };
+  }
+  else {
+      console.error(ynsTag + "Sorry, your browser doesn't support Web Workers! :/");
   }
 }
