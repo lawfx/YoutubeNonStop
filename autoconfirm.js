af15a7968feab1a196f3992e59db7778617020be
Nikolaos Ioannou <ioannounikos93@gmail.com>
2018-09-26 23:00:48 +0200

    page reloading now takes into account time spent on a video and applies it to the url

--- autoconfirm.js
+++ autoconfirm.js
@@ -1,6 +1,9 @@
 var ynsTag = "[Youtube NonStop] ";
 var clickTimeThreshold = 3000;
 var lastClickTime = new Date().getTime();
+var lastVideo = '';
+var timeStartedVideo = 0;
+var extraTime = 0;
 
 $(document).click(function() {
   lastClickTime = new Date().getTime();
@@ -24,13 +27,47 @@ function tryClickPaperDialog(){
       }
       if(paperDialog.find('#confirm-button').length){
         if(document.hidden){
-          document.location.reload();
-        } else{
+          checkForTimeStamp("replace");
+        }
+        else{
           paperDialog.find('#confirm-button').click();
           console.debug(ynsTag + "Confirmed watching in dialog!");
         }
       }
     }
+  }
+}
+
+function checkForTimeStamp(operation){
+  var url = window.location.origin + window.location.pathname;
+  var foundTimestamp = false;
+  window.location.search.split("&").forEach(function(curr, index){
+    if(index === 0){
+      curr = curr.split("?")[1];
+    }
+    var splits = curr.split("=");
+    if(splits[0] === "t"){
+      foundTimestamp = true;
+      if(operation === "get"){
+        extraTime = parseInt(splits[1]);
+      }
+      else if(operation === "replace"){
+        url += "&t=" + Math.floor((new Date() - timeStartedVideo) / 1000 + extraTime);
+      }
+    }
+    else if(operation === "replace"){
+      if(index === 0){
+        url += "?";
+      }
+      url += "&"+ curr;
+    }
+  });
+  if(operation === "replace"){
+    if(!foundTimestamp){
+      url += "&t=" + Math.floor((new Date() - timeStartedVideo) / 1000 + extraTime);
+    }
+    console.debug(ynsTag + "Cannot confirm watching because tab is hidden, reloading as " + url);
+    window.location.href = url;
   }
 }
 
@@ -60,6 +97,12 @@ window.onload=function(){
       worker.onmessage = function(e){
         if(e.data === "whip"){
           if(window.location.pathname === "/watch"){
+            if(lastVideo !== window.location.href){
+              extraTime = 0;
+              checkForTimeStamp("get");
+              lastVideo = window.location.href;
+              timeStartedVideo = new Date();
+            }
             tryClickPaperDialog();
           }
         }
